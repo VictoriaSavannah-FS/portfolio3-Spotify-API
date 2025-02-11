eaaa0950d5950f16504107d3fd4d173073da2083
Savannah Victoria <savannahcv@Savannahs-MacBook-Pro.local>
2025-02-10 20:15:09 -0600

    API init Commit

---
+++ routes/artistRoutes.js
@@ -0,0 +1,99 @@
+const express = require("express");
+const router = express.Router();
+// import .env & Schemas
+const Artist = require("../models/Artist");
+
+// MIDDLEWARE ----
+
+const getArtist = async (req, res, next) => {
+  let artist;
+  try {
+    artist = await Artist.findById(req.params.id);
+
+    // conditonals
+    if (artist === null) {
+      // iuf arttist does not exist/ no info
+      return res.status(404).json({ message: "Artist not found" });
+    }
+  } catch (error) {
+    //if all fails - server isseu w/ fetching
+    return res.status(500).json({ message: error.message });
+  }
+  res.artist = artist;
+  // since middleware--> next
+  next();
+};
+
+// RESTful Endpoints    --- --- --- --- --- --- ---
+//  GET ALL ========
+
+router.get("/", async (req, res) => {
+  // res.send("All Artists Route ---");
+  try {
+    const artists = await Artist.find();
+    return res.status(200).json(artists);
+  } catch (error) {
+    // server isues
+    return res.status(500).json({ message: error.message });
+  }
+});
+
+// GET ONE by ID -------use middlewaree
+router.get("/:id", getArtist, async (req, res) => {
+  res.send(`Artist ID: ${req.params.id} ---"`);
+});
+
+// POST ------------
+router.post("/:id", async (req, res) => {
+  const artist = new Artist({
+    name: req.body.name,
+    genres: req.body.genres,
+    popularity: req.body.popularity,
+    id: req.body.id,
+    images: req.body.images,
+    followers: req.body.followers,
+  });
+  // logic/cnditionls for respsne
+  try {
+    const postArtist = await artist.save();
+    // status -201 //Psot
+    res.status(201).json(postArtist);
+  } catch (error) {
+    // all fails
+    res.status(500).json({ message: error.message });
+  }
+});
+// PATCH -----------
+router.patch("/:id", getArtist, async (req, res) => {
+  // res.patch(`Artist ID: ${req.params.id} --- HAS BEEN EDITED"`);
+  if (req.body.name != null) {
+    res.artist.name = req.body.name;
+  }
+  if (req.body.genres != null) {
+    res.artist.genres = req.body.genres;
+  }
+
+  try {
+    const editArtist = await res.artist.save();
+    // status -200 //Psot
+    res.status(201).json(editArtist);
+  } catch (error) {
+    // all fails
+    res.status(500).json({ message: error.message });
+  }
+});
+// DELETE ----------
+router.delete("/:id", getArtist, async (req, res) => {
+  // res.send(`Artist ID: ${req.params.id} --- Has been DELETED"`);
+  try {
+    const deletedArtist = await res.artist.deleteOne();
+    res.status(200).json({
+      message: `Artist ${deletedArtist} withh ID: ${req.body.params} was DELETED succesfully---`,
+    });
+  } catch (error) {
+    // all fails
+    res.status(500).json({ message: error.message });
+  }
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,43 @@
+// setting up local server
+const express = require("express");
+require("dotenv").config(); //local config -> look for our envs. during builds
+// dev dependencies ------
+const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+// parse json req.s
+app.use(express.json());
+//PROT setup    -----
+const PORT = process.env.PORT || 8000; //|| heroku...
+
+const DATABASE_URL = process.env.DATABASE_URL;
+
+//mongoose - env. variabels ---- DATABAASE
+
+mongoose.connect(DATABASE_URL, {});
+const db = mongoose.connection;
+// error log----
+db.on("error", (error) => console.error(error));
+// db connects --- once on connction
+db.once("open", () =>
+  console.log(" --- | Database Connection Established | ---")
+);
+
+// mongoose.connect("mongodb://127.0.0.1:27017/spotifyx");
+
+// ROUTES -------------
+
+const artistRouter = require("./routes/artistRoutes");
+// everytioes we go to base url --- artists
+app.use("/artists", artistRouter);
+
+// listeners
+
+app.listen(PORT, () => {
+  console.log(
+    `Server is runnig on port: ${PORT} --- Yay! We're connected ---- `
+  );
+});
